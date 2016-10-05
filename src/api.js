62f06736269ed25da1c7269e9cda9bac203d3f6d
Edward Shih <ultralabed@gmail.com>
2016-10-05 12:29:34 +0800

    Add api fetch logic

--- src/api.js
+++ src/api.js
@@ -1,5 +1,18 @@
 const rootUrl = `api.openweathermap.org/data/2.5/weather?APPID=b12830e84c9325c4cf7c59dfea2627ab`; 
 
+const kelvinToC = function(kelvin) {
+  return Math.round(kelvin - 273.15) + '˚C';
+}
 module.exports = function(latitude, longitude) {
   let url = `${rootUrl}&lat=${latitude}&lon=${longitude}`;
+
+  fetch(url).then((response) => {
+    return response.json();
+  }).then((json) => {
+    return {
+      city: json.city,
+      temperature: kelvinToC(json.main.temp),
+      description: json.weather[0].description
+    };
+  });
 }
